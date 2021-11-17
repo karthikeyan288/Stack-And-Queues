66323816a2e795849d3ba7fc2cc8df64a9a5f937
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-17 22:47:10 +0530

    ADDED : the main fuction

---
+++ src/com/StackQueues/Main.java
@@ -0,0 +1,26 @@
+package com.StackQueues;
+
+public class Main {
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		stack.push(56);
+		stack.push(30);
+		stack.push(70);
+		stack.push(88);
+		stack.push(76);
+		stack.push(51);
+		
+		System.out.println(stack.peek());
+
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+		stack.pop();
+
+		stack.print();
+
+	}
+
+}
